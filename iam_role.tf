73db3f6ae7abbcf261eec6f343454484feae6602
Liubov Krivtsova <avostvirk@gmail.com>
2024-09-27 17:24:07 -0300

    Fix format

--- iam_role.tf
+++ iam_role.tf
@@ -2,18 +2,18 @@ resource "aws_iam_role" "GithubActionsRole" {
   name = "GithubActionsRole"
 
   assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
+    "Version" : "2012-10-17",
+    "Statement" : [
       {
-        "Effect": "Allow",
-        "Principal": {
-          "Federated": "arn:aws:iam::012345678910:oidc-provider/token.actions.githubusercontent.com"
+        "Effect" : "Allow",
+        "Principal" : {
+          "Federated" : "arn:aws:iam::012345678910:oidc-provider/token.actions.githubusercontent.com"
         },
-        "Action": "sts:AssumeRoleWithWebIdentity",
-        "Condition": {
-          "StringEquals": {
-            "token.actions.githubusercontent.com:aud": "sts.amazonaws.com",
-            "token.actions.githubusercontent.com:sub": "repo:liubov-js/rsschool-devops-course-tasks/*"
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com",
+            "token.actions.githubusercontent.com:sub" : "repo:liubov-js/rsschool-devops-course-tasks/*"
           }
         }
       }
